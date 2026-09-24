198ab1807db60a0b6bfd1fb0884acbdb2e89a0e7
Vince Mansel <vincemansel@gmail.com>
2026-09-24 15:15:10 -0700

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.swift
@@ -0,0 +1,36 @@
+class Solution {
+    func checkInclusion(_ s1: String, _ s2: String) -> Bool {
+        let s2 = Array(s2)
+
+        var count = [Character:Int]()
+        var window = [Character:Int]()
+        var l = 0
+
+        for c in s1 {
+            count[c, default: 0] += 1
+        }
+
+        for r in 0..<s2.count {
+            let c = s2[r]
+
+            if count.keys.contains(c) {
+                window[c, default: 0] += 1
+
+                if count == window {
+                    return true
+                }
+                
+                if (r - l + 1) >= s1.count {
+                    window[s2[l], default: 0] -= 1
+                    l += 1
+                }
+            }
+            else {
+                window = [Character:Int]()
+                l = r + 1
+            }
+        }
+
+        return false
+    }
+}
